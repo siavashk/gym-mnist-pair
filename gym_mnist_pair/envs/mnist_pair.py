34a08552dd5d65a146a38f787eb19b0cf90cfdfa
Siavash <siavash.khallaghi@clarius.me>
2017-11-15 23:30:27 -0800

    a little more further :)

--- gym_mnist_pair/envs/mnist_pair.py
+++ gym_mnist_pair/envs/mnist_pair.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def state_transition_from_direction(d):
     # Five actions for each block: nothing(0), right(1), up(2), left(3), down(4):
-    if type(d) != int || d < 0 || d > 4:
+    if type(d) != int or d < 0 or d > 4:
         raise ValueError('Unsupported action, got {}'.format(d))
         return (0, 0)
     else:
@@ -23,16 +23,16 @@ def state_transition_from_direction(d):
             return (-1, -1)
 
 def get_transition(action):
-'''
-    Five actions for each block: nothing(0), right(1), up(2), left(3), down(4):
-    For pair of blocks the following table of actions is available:
-    00 | 0    10 | 5    20 | 10    30 | 15    40 | 20
-    01 | 1    11 | 6    21 | 11    31 | 16    41 | 21
-    02 | 2    12 | 7    22 | 12    32 | 17    42 | 22
-    03 | 3    13 | 8    23 | 13    33 | 18    43 | 23
-    04 | 4    14 | 9    24 | 14    34 | 19    44 | 24
-'''
-    if type(action) != int || action < 0 || action > 24:
+    '''
+        Five actions for each block: nothing(0), right(1), up(2), left(3), down(4):
+        For pair of blocks the following table of actions is available:
+        00 | 0    10 | 5    20 | 10    30 | 15    40 | 20
+        01 | 1    11 | 6    21 | 11    31 | 16    41 | 21
+        02 | 2    12 | 7    22 | 12    32 | 17    42 | 22
+        03 | 3    13 | 8    23 | 13    33 | 18    43 | 23
+        04 | 4    14 | 9    24 | 14    34 | 19    44 | 24
+    '''
+    if type(action) != int or action < 0 or action > 24:
         raise ValueError('Unsupported action, got {}'.format(action))
         return (0, 0, 0, 0)
     else:
@@ -41,7 +41,7 @@ def get_transition(action):
         return np.array([st1[0], st1[1], st2[0], st2[1]])
 
 def load_mnist():
-    with open('train-images-idx3-ubyte', 'rb') as file:
+    with open('/home/siavash/personal/gym-mnist-pair/gym_mnist_pair/envs/train-images-idx3-ubyte', 'rb') as file:
         magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
         if magic != 2051:
             raise ValueError('Magic number mismatch, expected 2051, got {}'.format(magic))
@@ -57,22 +57,22 @@ def load_mnist():
 
 def makeTransMnist(image, inLength, outLength):
     image = image.reshape(inLength, inLength)
-    topLeft = random_integers(low=0, high=outLength - inLength, size=2)
+    topLeft = np.random.random_integers(low=0, high=outLength - inLength, size=2)
     transNist = np.zeros((outLength, outLength))
     transNist[topLeft[0]:topLeft[0]+inLength, topLeft[1]:topLeft[1]+inLength] = image
     return transNist
 
 def render_image(image, block, length, threshold):
     render = ''
-    for i in range(image.shape[0])):
-        for j in range(image.shape[1])):
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
             if image[i, j] > threshold:
-                if i >= block[0] && block[0] + length > i && j >= block[1] && block[1] + length > j:
+                if i >= block[0] and block[0] + length > i and j >= block[1] and block[1] + length > j:
                     render += "\x1b[1;%dm" % (30+1) + '@' + "\x1b[0m"
                 else:
                     render += '@'
             else:
-                if i >= block[0] && block[0] + length > i && j >= block[1] && block[1] + length > j:
+                if i >= block[0] and block[0] + length > i and j >= block[1] and block[1] + length > j:
                     render += "\x1b[1;%dm" % (30+1) + '.' + "\x1b[0m"
                 else:
                     render += '.'
@@ -107,10 +107,10 @@ class MnistPairEnv(gym.Env):
     def _reset(self):
         self.state = np.array([0, 0, 0, 0])
         idx = np.random.randint(0, self.number_of_images)
-        self.pair1 = makeTransMnist(self.mnist[idx, :])
-        self.pair2 = makeTransMnist(self.mnist[idx, :])
+        self.pair1 = makeTransMnist(self.mnist[idx, :], self.in_image_length, self.out_image_length)
+        self.pair2 = makeTransMnist(self.mnist[idx, :], self.in_image_length, self.out_image_length)
         self.current_step = 0
 
     def _render(self, mode='human', close=False):
-        render_image(self.pair1, self.state[0:2], self.in_image_length, 0)
-        render_image(self.pair2, self.state[2:4], self.in_image_length, 0)
+        print render_image(self.pair1, self.state[0:2], self.in_image_length, 0)
+        print render_image(self.pair2, self.state[2:4], self.in_image_length, 0)
